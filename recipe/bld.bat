5fe77fc409c87f60ce7243f0875695975f03106d
Isuru Fernando <isuruf@gmail.com>
2024-11-11 09:12:27 -0800

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,19 @@
-CP %BUILD_PREFIX%\Library\mingw-w64\bin\mingw32-make %BUILD_PREFIX%\Library\mingw-w64\bin\make
-mingw32-make lib
+make lib
 if errorlevel 1 exit 1
 
 cd MT\src\
-mingw32-make
+make
 if errorlevel 1 exit 1
 
-mkdir -p %LIBRARY_PREFIX%\mingw-w64\lib
-cp *.a %LIBRARY_PREFIX%\mingw-w64\lib\
+mkdir -p %LIBRARY_LIB%
+cp *.a %LIBRARY_LIB%
 if errorlevel 1 exit 1
 
 cd ..\..
-cp *.a %LIBRARY_PREFIX%\mingw-w64\lib\
+cp *.a %LIBRARY_LIB%
 if errorlevel 1 exit 1
 
-mkdir %LIBRARY_PREFIX%\mingw-w64\include\spooles
+mkdir %LIBRARY_INC%\spooles
 if errorlevel 1 exit 1
 
-xcopy /s .\*.h %LIBRARY_PREFIX%\mingw-w64\include\spooles
+xcopy /s .\*.h %LIBRARY_INC%\spooles
